33ab1ed9f6b2b78a9ea343d0313056dd38e0b2f9
Bharatshoray <bharatshorayrajora@gmail.com>
2024-03-18 23:54:47 +0530

    Initial commit

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)                  # colours ranging from blue to red 0-254
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+
+    # to add lines in blank page
+    # for y in range(20, 298, 10):
+    #    pdf.line(10, y, 200, y)
     pdf.line(10, 21, 200, 21)                    # pdf.line(x1, y1, x2, y2)
 
     # set the footer
@@ -24,9 +28,14 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):                   # this is a nested for loop and subtracting 1 will print correct
         pdf.add_page()                                  # no. of pages as the header add page will add one more page
 
+        # footer for next page
         pdf.ln(277)                                     # 277 = 265(ln of first page footer) + 12(h)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # to add line in blank page
+        # for y in range(20, 298, 10):
+        #    pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
